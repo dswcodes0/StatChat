88b1862f93a8a51c03f73b45dcdfa46c034fdff9
Darren Washington <dsw@meta.com>
2023-07-20 12:09:42 -0700

    completed sign up feature

--- statchatui/src/SignUp/SignUp.jsx
+++ statchatui/src/SignUp/SignUp.jsx
@@ -5,10 +5,29 @@ const SignUp = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = {
-      username: event.target.username.value,
-      email: event.target.email.value,
-      password: event.target.password.value,
+      Username: event.target.username.value,
+      Email: event.target.email.value,
+      Password: event.target.password.value,
     };
+    try {
+      const response = await fetch("http://localhost:3002/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert("Sign-up successful!");
+        //FIXME REDIRECT USER TO HOME WITH THE CREDENTIALS ENTERED
+      } else {
+        const errorData = await response.json();
+        alert(`Sign-up failed: ${errorData.message}`);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
   };
   return (
     <div className="container">
