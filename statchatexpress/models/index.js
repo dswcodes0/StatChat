4758ebf6dca564339c10d95d0599a54afbe8ef89
Darren Washington <dsw@meta.com>
2023-07-20 10:20:34 -0700

    changed has many

--- statchatexpress/models/index.js
+++ statchatexpress/models/index.js
@@ -1,7 +1,7 @@
-import { User } from './user.js';
-import { Stats } from './stats.js';
+import { User } from "./user.js";
+import { Stats } from "./stats.js";
 
-User.hasOne(Stats, { as: 'stats', foreignKey: 'userId' });
-Stats.belongsTo(User, { as: 'user', foreignKey: 'userId' });
+User.hasMany(Stats, { as: "stats", foreignKey: "userId" });
+Stats.belongsTo(User, { as: "user", foreignKey: "userId" });
 
 export { User, Stats };
