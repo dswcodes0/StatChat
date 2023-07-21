c92923a0c95d7a1ba38747a234ff53b70042dd7f
Darren Washington <dsw@meta.com>
2023-07-20 19:44:14 -0700

    completed login functionality

---
+++ statchatui/src/SignIn/SignIn.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css";
+
+const SignIn = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      Username: event.target.username.value,
+      Password: event.target.password.value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3002/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        alert("login succesful");
+      } else {
+        alert(`Login failed: ${data.message}`);
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+  return (
+    <div className="container">
+      <h1>Let's get back into it!</h1>
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          className="input-field"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="input-field"
+        />
+        <input type="submit" value="Submit" className="submit-btn" />
+      </form>
+      <Link to="/SignUp" className="sign-in-redirect">
+        Create An Account
+      </Link>
+    </div>
+  );
+};
+export default SignIn;
