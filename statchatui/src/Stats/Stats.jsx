0fc7fa714fc470556009933f7c1d346ba269ebe4
Darren Washington <dsw@meta.com>
2023-07-24 14:00:53 -0700

    changed it to return early to not check twice

--- statchatui/src/Stats/Stats.jsx
+++ statchatui/src/Stats/Stats.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 
 const Stats = ({ statData }) => {
-  let kills, level, name, rank;
-  if (statData != null) {
-    kills = statData.total.kills.value;
-    level = statData.global.level;
-    name = statData.global.name;
-    rank = statData.global.rank.rankName;
+  if (!statData) {
+    return <div>Stats not yet fetched.</div>;
   }
+
+  let kills, level, name, rank;
+  kills = statData.total.kills.value;
+  level = statData.global.level;
+  name = statData.global.name;
+  rank = statData.global.rank.rankName;
 
   return (
     <div className="container">
-      {statData ? (
-        <div>
-          <h1>Apex Legends</h1>
-          <h2>Stats:</h2>
-          <h3>Name: {name}</h3>
-          <h3>Level: {level}</h3>
-          <h3>Rank: {rank}</h3>
-          <h3>Kills: {kills}</h3>
-        </div>
-      ) : (
-        <div>Stats not yet fetched.</div>
-      )}
+      <div>
+        <h1>Apex Legends</h1>
+        <h2>Stats:</h2>
+        <h3>Name: {name}</h3>
+        <h3>Level: {level}</h3>
+        <h3>Rank: {rank}</h3>
+        <h3>Kills: {kills}</h3>
+      </div>
     </div>
   );
 };
