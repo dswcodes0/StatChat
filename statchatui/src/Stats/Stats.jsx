7996db48bc8c794b2db131e9e8e25bcd9abab809
Darren Washington <dsw@meta.com>
2023-07-24 11:55:04 -0700

    added more stats data

--- statchatui/src/Stats/Stats.jsx
+++ statchatui/src/Stats/Stats.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 const Stats = ({ statData }) => {
+  console.log(statData);
   const kills = statData?.total?.kills?.value;
+  const level = statData?.global?.level;
+  const name = statData?.global?.name;
+  const rank = statData?.global?.rank?.rankName;
+
   return (
     <div className="container">
       {statData ? (
         <div>
           <h1>Stats:</h1>
-          <p>Kills: {kills}</p>
+          <h3>Name: {name}</h3>
+          <h3>Level: {level}</h3>
+          <h3>Rank: {rank}</h3>
+          <h3>Kills: {kills}</h3>
         </div>
       ) : (
         <div>Loading...</div>
