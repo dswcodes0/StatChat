f1e8189a3567353d522c9927296936779d0975e7
Darren Washington <dsw@meta.com>
2023-07-24 13:49:50 -0700

    added null checker

--- statchatui/src/Stats/Stats.jsx
+++ statchatui/src/Stats/Stats.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
 const Stats = ({ statData }) => {
-  console.log(statData);
-  const kills = statData?.total?.kills?.value;
-  const level = statData?.global?.level;
-  const name = statData?.global?.name;
-  const rank = statData?.global?.rank?.rankName;
+  let kills, level, name, rank;
+  if (statData != null) {
+    console.log(statData);
+    kills = statData.total.kills.value;
+    level = statData.global.level;
+    name = statData.global.name;
+    rank = statData.global.rank.rankName;
+  }
 
   return (
     <div className="container">
       {statData ? (
         <div>
-          <h1>Stats:</h1>
+          <h1>Apex Legends</h1>
+          <h2>Stats:</h2>
           <h3>Name: {name}</h3>
           <h3>Level: {level}</h3>
           <h3>Rank: {rank}</h3>
           <h3>Kills: {kills}</h3>
         </div>
       ) : (
-        <div>Loading...</div>
+        <div>Stats not yet fetched.</div>
       )}
     </div>
   );
