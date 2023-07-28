98eddb2edddf3f654637a83365e9f911ed0e229e
Darren Washington <dsw@meta.com>
2023-07-28 10:57:55 -0700

    testing bug

--- statchatexpress/models/userGame.js
+++ statchatexpress/models/userGame.js
@@ -1,29 +1,18 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database.js";
 
-export const UserGame = sequelize.define(
-  "UserGame",
-  {
-    Gamertag: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    Platform: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    GameName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: false,
-    },
+export const UserGame = sequelize.define("UserGame", {
+  Gamertag: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  {
-    indexes: [
-      {
-        unique: true,
-        fields: ["UserId", "GameName", "Platform"],
-      },
-    ],
-  }
-);
+  Platform: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  GameName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+  },
+});
