70631b843ac6755e3703a452ace9014bcbe80b3c
Darren Washington <dsw@meta.com>
2023-07-28 14:47:41 -0700

    reverted changes to stats file

--- statchatui/src/Stats/Stats.jsx
+++ statchatui/src/Stats/Stats.jsx
@@ -2,28 +2,24 @@ import React from "react";
 
 const Stats = ({ statData }) => {
   if (!statData) {
-    return <div>{"Stats not yet fetched."}</div>;
+    return <div>Stats not yet fetched.</div>;
   }
 
   let kills, level, name, rank;
-  if (statData.total) {
-    kills = statData.total.kills.value;
-  }
-  if (statData.global) {
-    level = statData.global.level;
-    name = statData.global.name;
-    rank = statData.global.rank.rankName;
-  }
+  kills = statData.total.kills.value;
+  level = statData.global.level;
+  name = statData.global.name;
+  rank = statData.global.rank.rankName;
 
   return (
     <div className="container">
       <div>
         <h1>Apex Legends</h1>
         <h2>Stats:</h2>
-        {name && <h3>Name: {name}</h3>}
-        {level && <h3>Level: {level}</h3>}
-        {rank && <h3>Rank: {rank}</h3>}
-        {kills && <h3>Kills: {kills}</h3>}
+        <h3>Name: {name}</h3>
+        <h3>Level: {level}</h3>
+        <h3>Rank: {rank}</h3>
+        <h3>Kills: {kills}</h3>
       </div>
     </div>
   );
