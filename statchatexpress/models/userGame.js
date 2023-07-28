4121143911494b30e02b8c173e6fd25b582d5a7b
Darren Washington <dsw@meta.com>
2023-07-28 10:47:28 -0700

    fixed a {} formatting error

--- statchatexpress/models/userGame.js
+++ statchatexpress/models/userGame.js
@@ -1,24 +1,29 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database.js";
 
-export const UserGame = sequelize.define("UserGame", {
-  Gamertag: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Platform: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  GameName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: false,
-  },
-  indexes: [
-    {
-      unique: true,
-      fields: ["UserId", "GameName", "Platform"],
+export const UserGame = sequelize.define(
+  "UserGame",
+  {
+    Gamertag: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-  ],
-});
+    Platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    GameName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["UserId", "GameName", "Platform"],
+      },
+    ],
+  }
+);
