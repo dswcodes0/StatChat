aef923c5e0d02f81c0088178ff83db7a7b05c1a2
Darren Washington <dsw@meta.com>
2023-07-31 10:23:01 -0700

    made the usergame entries unique.

--- statchatexpress/models/userGame.js
+++ statchatexpress/models/userGame.js
@@ -1,17 +1,30 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database.js";
 
-export const UserGame = sequelize.define("UserGame", {
-  Gamertag: {
-    type: DataTypes.STRING,
-    allowNull: false,
+export const UserGame = sequelize.define(
+  "UserGame",
+  {
+    Gamertag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    GameName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //user can only have one account per game:
+      unique: true,
+    },
   },
-  Platform: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  GameName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["Gamertag", "GameName", "Platform"],
+      },
+    ],
+  }
+);
