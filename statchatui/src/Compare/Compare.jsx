0a608bdffca5a2d18c70279de7f8f54e98eaf1b8
Darren Washington <dsw@meta.com>
2023-08-09 16:16:46 -0700

    added more constraints to be added to userqueue

--- statchatui/src/Compare/Compare.jsx
+++ statchatui/src/Compare/Compare.jsx
@@ -43,34 +43,45 @@ const Compare = ({ signedInUserData }) => {
     setIsLoading(true); //sets the state to true before the api call is displaying the stats
     let stats1;
     let stats2;
+    let validUser = true;
     try {
       stats1 = await fetchStats(user1);
       stats2 = await fetchStats(user2);
     } catch (error) {
       setError(error?.message);
+      validUser = false;
+    }
+    //if there was an error fetching the stats( meaning that the user does not exist) then it will not be added to the user queue
+    if (validUser) {
+      setOtherUserStats(stats1);
+      setSignedInUserStats(stats2);
+      setOtherUserFormData(user1);
+      setSignedInUserFormData(user2);
+
+      //checks if the user is in the userqueue already
+      const isUserInQueue = userQueue.some(
+        (user) => user.gamertag === user1.gamertag
+      );
+      if (!isUserInQueue) {
+        const newUser = {
+          gamertag: user1.gamertag,
+          platform: user1.platform,
+          gameName: user1.gameName,
+          // Assigned the newUser to have the stats of stats1 because otherUser and newUser are the same and I want the stats to be saved in the userQueue
+          stats: stats1,
+        };
+
+        setUserQueue((prevQueue) => {
+          const updatedQueue = [...prevQueue, newUser];
+          // if the queue size is over three, remove oldest user which is the first element of the array
+          if (updatedQueue.length > 3) {
+            updatedQueue.shift();
+          }
+          return updatedQueue;
+        });
+      }
     }
 
-    setOtherUserStats(stats1);
-    setSignedInUserStats(stats2);
-    setOtherUserFormData(user1);
-    setSignedInUserFormData(user2);
-
-    const newUser = {
-      gamertag: user1.gamertag,
-      platform: user1.platform,
-      gameName: user1.gameName,
-      // Assigned the newUser to have the stats of stats1 because otherUser and newUser are the same and I want the stats to be saved in the userQueue
-      stats: stats1,
-    };
-
-    setUserQueue((prevQueue) => {
-      const updatedQueue = [...prevQueue, newUser];
-      // if the queue size is over three, remove oldest user which is the first element of the array
-      if (updatedQueue.length > 3) {
-        updatedQueue.shift();
-      }
-      return updatedQueue;
-    });
     setIsLoading(false);
   };
 
