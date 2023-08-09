0f3ac80e973207cc1f2007b34faf2e49a107554f
Darren Washington <dsw@meta.com>
2023-08-09 11:16:13 -0700

    completed compare stats on click

--- statchatui/src/Compare/Compare.jsx
+++ statchatui/src/Compare/Compare.jsx
@@ -41,9 +41,38 @@ const Compare = ({ signedInUserData }) => {
   //   const stats1 = await fetchStats(userStats1);
   //   const stats2 = await fetchStats(userStats2);
   // };
+
+  const compareUsers = async (user1, user2) => {
+    setIsLoading(true); //sets the state to true before the api call is displaying the stats
+    const stats1 = await fetchStats(user1);
+    const stats2 = await fetchStats(user2);
+
+    setOtherUserStats(stats1);
+    setSignedInUserStats(stats2);
+    setOtherUserFormData(user1);
+    setSignedInUserFormData(user2);
+
+    const newUser = {
+      gamertag: user1.gamertag,
+      platform: user1.platform,
+      gameName: user1.gameName,
+      // Assigned the newUser to have the stats of stats1 because otherUser and newUser are the same and I want the stats to be saved in the userQueue
+      stats: stats1,
+    };
+
+    setUserQueue((prevQueue) => {
+      const updatedQueue = [...prevQueue, newUser];
+      // if the queue size is over three, remove oldest user which is the first element of the array
+      if (updatedQueue.length > 3) {
+        updatedQueue.shift();
+      }
+      return updatedQueue;
+    });
+    setIsLoading(false);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setIsLoading(true); //sets the state to true before the api call is displaying the stats
 
     const gamertag = event.target.gamertag.value;
     const platform = event.target.platform.value;
@@ -59,31 +88,7 @@ const Compare = ({ signedInUserData }) => {
       platform,
       gameName,
     };
-    const stats1 = await fetchStats(submitData);
-    const stats2 = await fetchStats(signedInUserData);
-    setOtherUserStats(stats1);
-    setSignedInUserStats(stats2);
-    setOtherUserFormData(submitData);
-    setSignedInUserFormData(signedInUserData);
-    setIsLoading(false);
-
-    //this is the userdata that will be used to populate the queue with previous users
-    const newUser = {
-      gamertag: submitData.gamertag,
-      platform: submitData.platform,
-      gameName: submitData.gameName,
-      // Assigned the newUser to have the stats of stats1 because otherUser and newUser are the same and I want the stats to be saved in the userQueue
-      stats: stats1,
-    };
-
-    setUserQueue((prevQueue) => {
-      const updatedQueue = [...prevQueue, newUser];
-      // if the queue size is over three, remove oldest user which is the first element of the array
-      if (updatedQueue.length > 3) {
-        updatedQueue.shift();
-      }
-      return updatedQueue;
-    });
+    compareUsers(submitData, signedInUserData);
   };
 
   const onGamertagChange = (event) => {
@@ -149,6 +154,9 @@ const Compare = ({ signedInUserData }) => {
               className="previous-user"
               onMouseOver={() => setShowStats(index)}
               onMouseOut={() => setShowStats(null)}
+              onClick={() => {
+                compareUsers(user, signedInUserData);
+              }}
             >
               {user.gamertag}
             </h1>
